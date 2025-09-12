21a671baafbb4da193c98454cf0aaa1f50884e3a
GAOSHIQING <ab1314625@qq.com>
2025-09-12 15:16:55 +0800

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -2,17 +2,20 @@
 from .node.load_svg import LoadSVG
 from .node.split_mask import MaskSplitterPro
 from .node.svg_to_image import SVGToImage
+from .node.custom_load_image import CustomLoadImageWithFormat
 
 NODE_CLASS_MAPPINGS = {
     "Load SVG": LoadSVG,
     "MaskSplitterPro": MaskSplitterPro,
     "SVG To Image": SVGToImage,
+    "CustomLoadImageWithFormat": CustomLoadImageWithFormat,
 }
 
 NODE_DISPLAY_NAME_MAPPINGS = {
     "Load SVG": "Load SVG",
     "MaskSplitterPro": "Split Mask",
     "SVG To Image": "SVG To Image",
+    "CustomLoadImageWithFormat": "加载图像(支持SVG)",
 }
 
 __all__ = ["NODE_CLASS_MAPPINGS", "NODE_DISPLAY_NAME_MAPPINGS"]
