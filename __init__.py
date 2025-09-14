a5b5b7cf9aae979291f7f03bc62bb366a3960b2c
GAOSHIQING <ab1314625@qq.com>
2025-09-14 19:53:29 +0800

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -3,12 +3,16 @@ from .nodes.load_svg import LoadSVG
 from .nodes.split_mask import MaskSplitterPro
 from .nodes.svg_to_image import SVGToImage
 from .nodes.custom_load_image import CustomLoadImageWithFormat
+from .nodes.mask_scale import MaskScale
+from .nodes.text_compare import TextCompare
 
 NODE_CLASS_MAPPINGS = {
     "Load SVG": LoadSVG,
     "MaskSplitterPro": MaskSplitterPro,
     "SVG To Image": SVGToImage,
     "CustomLoadImageWithFormat": CustomLoadImageWithFormat,
+    "MaskScale": MaskScale,
+    "TextCompare": TextCompare,
 }
 
 NODE_DISPLAY_NAME_MAPPINGS = {
@@ -16,6 +20,8 @@ NODE_DISPLAY_NAME_MAPPINGS = {
     "MaskSplitterPro": "拆分遮罩",
     "SVG To Image": "SVG到图像",
     "CustomLoadImageWithFormat": "加载图像(支持SVG)",
+    "MaskScale": "遮罩缩放",
+    "TextCompare":"文本比较",
 }
 
 __all__ = ["NODE_CLASS_MAPPINGS", "NODE_DISPLAY_NAME_MAPPINGS"]
